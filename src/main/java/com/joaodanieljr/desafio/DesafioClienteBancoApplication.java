84be5a3ea575f2169b5db462ffa90e7d0b45f349
Joao Junior <Joaodanieljr@gmail.com>
2020-12-28 00:41:31 -0300

    Projeto finalizando

---
+++ src/main/java/com/joaodanieljr/desafio/DesafioClienteBancoApplication.java
@@ -0,0 +1,13 @@
+package com.joaodanieljr.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioClienteBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioClienteBancoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/joaodanieljr/desafio/services/ClienteService.java
@@ -0,0 +1,75 @@
+package com.joaodanieljr.desafio.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.joaodanieljr.desafio.domain.Cliente;
+import com.joaodanieljr.desafio.dto.ClienteDTO;
+import com.joaodanieljr.desafio.exceptions.ObjectNotFoundException;
+import com.joaodanieljr.desafio.repository.ClienteRepository;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+																"Objeto não encontrado! ID: " 
+																+ id 
+																+ ", Tipo: " 
+																+ Cliente.class.getName())); 
+		
+		
+
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		return obj;
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj =  find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		repo.deleteById(id);
+	}
+	
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pagerequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pagerequest);
+	}
+	
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), objDTO.getCpf(), objDTO.getDataNascimento());
+	}
+	
+	
+	private void updateData (Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		newObj.setCpf(obj.getCpf());
+		newObj.setDataNascimento(obj.getDataNascimento());
+	}
+}
